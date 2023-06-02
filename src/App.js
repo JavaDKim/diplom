3222e2f3e6fd2ea95a6f120d71670e454c48eeed
dmitry kim <javascriptkim@yandex.ru>
2023-06-02 17:55:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import ControlledCarousel from './components/Carousel';
+import NavbarMenu from './components/NavbarHeader';
+import NavbarFilter from './components/NavbarFilter';
+import PostPopular from './components/PostPopular';
+import PostNew from './components/PostNew';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+	return (
+
+	<Container>
+		<Row className='header'><NavbarMenu/><ControlledCarousel/></Row>
+		<Row><NavbarFilter/></Row>
+		<Row>
+			<Form className='d-flex justify-content-center'>
+					<h3 className='mt-5'>Популярное</h3>
+			</Form>
+			<Row className='justify-content-beetwen m-0 p-0 mt-5'>
+					<Col className='justify-content-center'> <PostPopular img={"popular1.png"}/> </Col>
+					<Col className='justify-content-center'><PostPopular img={"popular2.png"}/></Col>
+			</Row>
+		</Row>
+		<Form className='d-flex justify-content-center'>
+					<h3 className='mt-5'>Новые публикации</h3>
+			</Form>
+		<Row className='justify-content-beetwen m-0 p-0 mt-5'>
+		  <Col className='justify-content-center'><PostNew img={"post1.png"}/> </Col>
+			<Col className='justify-content-center'><PostNew img={"post2.png"}/></Col>
+			<Col className='justify-content-center'><PostNew img={"post3.png"}/></Col>
+		</Row>
+		<Row className='justify-content-beetwen m-0 p-0 mt-3'>
+		  <Col className='justify-content-center'><PostNew img={"post4.png"}/> </Col>
+			<Col className='justify-content-center'><PostNew img={"post5.png"}/></Col>
+			<Col className='justify-content-center'><PostNew img={"post6.png"}/></Col>
+		</Row>
+
+		<Row>Футер</Row>
+	</Container>
   );
 }
 
