fdb7ecf4633357b308a60eed30b79d51fe1beaa0
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-06-16 21:32:10 +0300

    Edit NavbarFooter

--- src/components/NavbarFooter/index.jsx
+++ src/components/NavbarFooter/index.jsx
@@ -1,40 +1,20 @@
 import "./style.css"
-import { useContext, useEffect, useState } from "react";
-import { Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas, Row } from 'react-bootstrap';
+import { useContext } from "react";
+import { Container, Nav, Navbar, Offcanvas, Row } from 'react-bootstrap';
 import AppCtx from "../../context"
 import { useNavigate } from "react-router-dom";
 // икноки из библиотеки mui //
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
-import GroupIcon from '@mui/icons-material/Group';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 function NavbarMenu() {
 	const {
-		setUser,
 		userId,
-		setUserId,
-		setToken,
-		postsSrv,
-		setPostsSrv,
-		textSearch,
-		setTextSearch
+
 	} = useContext(AppCtx);
 	const navigate = useNavigate()
-	const exit = (e) => {
-		e.preventDefault()
-		setUser("")
-		setUserId("")
-		setToken("")
-		localStorage.removeItem("travelPostsAll");
-		localStorage.removeItem("travelBlogUser");
-		localStorage.removeItem("travelBlogToken");
-		localStorage.removeItem("travelBlogId");
-		localStorage.removeItem("travelBlogUserInfo");
-		navigate("/")
-	}
+
 
 	const postsFavor = (e) => {
 		e.preventDefault()
