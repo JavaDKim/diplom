9b51e1c5326dcfe2f8b21ed774b1a84aa2ef4f55
dmitry kim <javascriptkim@yandex.ru>
2023-06-18 13:52:18 +0300

    edit main pages and new, popular posts, carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "./carousel.css"
-
+import { carouselArray } from "../../assets/mainPosts"
 
 function ControlledCarousel() {
 	const [index, setIndex] = useState(0);
@@ -12,43 +12,17 @@ function ControlledCarousel() {
 
 	return (
 		<Carousel activeIndex={index} onSelect={handleSelect} className='carousel'>
-			<Carousel.Item>
+			{carouselArray?.map(e => <Carousel.Item>
 				<img
 					className="d-block w-100"
-					src={require('../../assets/images/main2.png')}
+					src={require(`../../assets/images/${e.img}`)}
 					alt="First slide"
 				/>
-				<Carousel.Caption>
-					<h3>"ПАРК ГАЛИЦКОГО". РОССИЯ. Краснодар</h3>
-					<p>Известный меценат и предприниматель потратил на парк 4 млрд рублей — и развитие идет до сих пор. В результате пустырь рядом со стадионом превратился в большой современный парк, который теперь называют обязательной к посещению достопримечательностью и произведением искусства.</p>
+				<Carousel.Caption >
+					<h4 className="carouselTitle">{e.title}</h4>
+					<span className="carouselText">{e.text}</span>
 				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={require('../../assets/images/main1.png')}
-					alt="Second slide"
-				/>
-
-				<Carousel.Caption>
-					<h3>ГОРА ЭЛЬБРУС. РОССИЯ. КАБАРДИНО-БАЛКАРСКАЯ РЕСПУБЛИКА</h3>
-					<p> Эльбрус протыкает макушкой небо чуть севернее Главного Кавказского хребта. Здесь он появился примерно 2-3 миллиона лет назад. За это время Эльбрус дорос до того, чтобы стать самой высокой горной вершиной Европы и попасть в список высочайших гор шести частей света «Семь вершин».</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={require('../../assets/images/main3.png')}
-					alt="Third slide"
-				/>
-
-				<Carousel.Caption>
-					<h3>БАРХАН САРЫ-КУМ. РОССИЯ. ДАГЕСТАН</h3>
-					<p>
-						В 18 километрах от Махачкалы, в предгорьях хребта Нарат-Тюбе расположен удивительный памятник природы – огромный песчаный бархан Сары-Кум.
-					</p>
-				</Carousel.Caption>
-			</Carousel.Item>
+			</Carousel.Item>)}
 		</Carousel>
 	);
 }
